5a0e3e7ceb2eb5d6843e4b15fa9cf8023e981e25
kcs kishore chowdary <kcs.kishore1997@gmail.com>
2022-03-03 10:45:17 +0530

    Add project files.

---
+++ InvalidRootDescriptionException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FileSearch
+{
+    [Serializable]
+    internal class InvalidRootDescriptionException : Exception
+    {
+        public InvalidRootDescriptionException()
+        {
+        }
+
+        public InvalidRootDescriptionException(string message) : base(message)
+        {
+        }
+
+        public InvalidRootDescriptionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidRootDescriptionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
