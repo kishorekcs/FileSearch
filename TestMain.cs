2c83b418b80c7d927376a9161fd30709f5e3e555
kcs kishore chowdary <kcs.kishore1997@gmail.com>
2022-03-03 14:53:34 +0530

    subham

--- TestMain.cs
+++ TestMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FileSearch.Class;
 
 namespace FileSearch
 {
@@ -10,7 +11,48 @@ namespace FileSearch
     {
         static void Main(string[] args)
         {
+            #region Notes
 
+            /*
+            Approach
+
+            1. Goals
+                Fastest way to find a file in a system
+            2. What do we need to do - Scope of work
+
+                Abstraction - **** Important Word - What
+                Implementation - How
+                Class- Who
+                ADT - Abstraction Based Technique
+                NIT - Noun Identification Technique
+                DDT - Data Driven Technique
+
+                1. Seach for file
+                    1.1 : Accept File Name
+                    1.2 : Accept the drives to search in
+                    1.3 : Check if file has already been searched in history
+                    1.4 : If yes, display
+                    1.5 : If no, Initiate new search 
+                    1.6 : Search for file
+                    1.6.1 : Search for file in a multi-threaded manner
+                    1.6.2 : Capture the results
+                2. Store the search results in history
+                3. Display search results
+            */
+
+            /*
+                Different category of classes
+                Boundary
+                Control
+                Entity
+                Extenal Interface
+            */
+
+            #endregion
+
+            var searchForm = new SearchInputForm();
+
+            searchForm.Search();
         }
     }
 }
