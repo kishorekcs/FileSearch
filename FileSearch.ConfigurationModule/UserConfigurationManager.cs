82c5379d66141be1976610f75f88b54e65901f77
kcs kishore chowdary <kcs.kishore1997@gmail.com>
2022-03-07 13:01:30 +0530

    ConfigurationModule start

---
+++ FileSearch.ConfigurationModule/UserConfigurationManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearch.ConfigurationModule
+{
+    public interface UserConfigurationManager
+    {
+        #region task
+        /*
+         a) Create user
+         b) Edit user
+         c) 
+         */
+        #endregion
+    }
+}
